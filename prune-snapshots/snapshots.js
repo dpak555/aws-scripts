6df7182f7146ee3a8bdb02798554b90009a34702
Ville Walveranta <ville@walveranta.org>
2017-06-13 03:06:29 -0500

    Automated and included detached volume detection/deletion.

--- prune-snapshots/snapshots.js
+++ prune-snapshots/snapshots.js
@@ -1,10 +1,7 @@
 
 'use strict';
 
-var RateLimiter = require('limiter').RateLimiter;
-// Allow 2 requests per second. Also understands
-// 'second', 'minute', 'day', or a number of milliseconds
-var limiter = new RateLimiter(2, 'second');
+const OwnerIDs = [ 'XXXXXXXXXXXX' ];
 
 const util = require('util');
 const _ = require('lodash');
@@ -14,80 +11,102 @@ const moment = require('moment');
 const jsonfile = require('jsonfile');
 var AWS = require('aws-sdk');
 AWS.config.loadFromPath('./config.json');
-const statefile = './snapshots-pending-delete.json';
+const snapStateFile = './snapshots-pending-delete.json';
+const volStateFile = './volumes-pending-delete.json';
 
 var ec2 = new AWS.EC2();
+var deletionTime = moment().add(1, 'week').unix();
+var deleteObj = {};
 
 var activeSourceSnapshots = new Set();
-var orphanCollector = new Set();
-var orphanCollectorArr = [];
+var orphanSnapCollector = new Set();
+var orphanSnapCollectorArr = [];
 var sourceSnapshotDetail = [];
 var sourceVolumeDetail = [];
 var pitSnapshotsCollectorArr = [];
 var deletablePitSnapshots = [];
-var excludedByDND = [];
+var snapsExcludedByDND = [];
+var delVolCollector = [];
+var volsExcludedByDND = [];
 
-function saveState(state) {
-  jsonfile.writeFile(statefile, state, function (err) {
+// save persisted state into a file
+function saveState(state, stateFile) {
+  jsonfile.writeFile(stateFile, state, function (err) {
     if (err) console.error(err);
   })
 }
 
-function loadState() {
+// read persisted state from a file (check if it exists first)
+function loadState(stateFile) {
   var deletables = [];
-  if (fs.existsSync(statefile)) {
-    deletables = jsonfile.readFileSync(statefile);
+  if (fs.existsSync(stateFile)) {
+    try {
+      deletables = jsonfile.readFileSync(stateFile);
+    }
+    // ignore errors; just return an empty array, and the new state will overwrite it
+    catch (e) {}
   }
   return deletables;
 }
 
-function deleteBy(deleteByArr, deleteArr, deleteArrKey, deleteByKey) {
+// delete items/objects in an array by properties of another array
+function deleteBy(deleteByArr, deleteArr, deleteArrKey, deleteByKey, deltype) {
   var delidx
   for (delidx = deleteByArr.length - 1; delidx >= 0; delidx--) {
-    deleteArr.splice(_.findIndex(deleteArr, function (item) {
-      return item[deleteArrKey] === deleteByArr[delidx][deleteByKey];
+    deleteArr.splice(_.findIndex(deleteArr, function(item) {
+      if (deltype == 'delby2delarr1') {
+        return item === deleteByArr[delidx][deleteByKey];
+      }
+      else if (deltype == 'delby1delarr2') {
+        return item[deleteArrKey] === deleteByArr[delidx];
+      }
     }), 1);
   }
   return deleteArr;
 }
 
 async.parallel({
-    Volumes: function(callback) {
+  Volumes: function(callback) {
 
-      // acquire in-use volumes
-      var paramsDescVol = {};
-      ec2.describeVolumes(paramsDescVol, function(err, data) {
-        if (err) console.log(err, err.stack); // an error occurred
-        else {
-          callback(null, data);
+    // acquire in-use and available (detached) volumes
+    // (do not select volumes that are being created or deleted)
+    var paramsDescVol = {
+      Filters: [
+        {
+          Name: 'status',
+          Values: [ 'available', 'in-use' ]
         }
-      });
-    },
+      ]    
+    };
+    ec2.describeVolumes(paramsDescVol, function(err, data) {
+      if (err) console.log(err, err.stack); // an error occurred
+      else {
+        callback(null, data);
+      }
+    });
+  },
 
-    Snapshots: function(callback) {
+  Snapshots: function(callback) {
 
-      // acquire my completed snapshots
-      var paramsDescSnap = {
-        Filters: [
-          {
-            Name: "status", 
-            Values: [
-              "completed"
-            ]
-          }
-        ],
-        OwnerIds: [
-          "XXXXXXXXXXXX"
-        ]
-      };
-
-      ec2.describeSnapshots(paramsDescSnap, function(err, data) {
-        if (err) console.log(err, err.stack); // an error occurred
-        else {
-          callback(null, data);
+    // acquire my completed snapshots
+    var paramsDescSnap = {
+      Filters: [
+        {
+          Name: 'status', 
+          Values: [
+            'completed'
+          ]
         }
-      });
-    }
+      ],
+      OwnerIds: OwnerIDs
+    };
+     ec2.describeSnapshots(paramsDescSnap, function(err, data) {
+      if (err) console.log(err, err.stack); // an error occurred
+      else {
+        callback(null, data);
+      }
+    });
+  }
 },
 function(err, results) {
 
@@ -95,7 +114,7 @@ function(err, results) {
 //  console.log(util.inspect(results.Volumes.Volumes, {showHidden: false, depth: null}));
 //  console.log(util.inspect(results.Snapshots.Snapshots, {showHidden: false, depth: null}));
 
-  // iterate snapshots
+  // ITERATE SNAPSHOTS
   _.forEach(results.Snapshots.Snapshots, function(snapshot) {
 
     var vol_match = false;
@@ -103,7 +122,7 @@ function(err, results) {
 
     // collect Do Not Delete (DND) tags for later exclusion
     if (_.find(snapshot.Tags, { 'Key': 'DND', 'Value': 'true' })) {
-      excludedByDND.push(snapshot.SnapshotId);
+      snapsExcludedByDND.push(snapshot.SnapshotId);
     }
 
     // iterate volumes for each snapshot
@@ -150,7 +169,7 @@ function(err, results) {
 
     if (!vol_match && !snap_match) {
       // This is an orphan snapshot (no source, no destination), and must be deleted!
-      orphanCollector.add(snapshot.SnapshotId);
+      orphanSnapCollector.add(snapshot.SnapshotId);
     }
     
   });
@@ -160,12 +179,32 @@ function(err, results) {
   activeSourceSnapshots = '';
 
   // array of orphan snapshots (these are deletable as-is)
-  orphanCollectorArr = Array.from(orphanCollector);
-  orphanCollector = '';
+  orphanSnapCollectorArr = Array.from(orphanSnapCollector);
+  orphanSnapCollector = '';
 
-  // iterate volumes to find all point-in-time snapshots for each volume
+  // do not consider detached volumes younger than 2 days for deletion
+  var volSelectTimeLimiter = moment().subtract(2, 'days').unix();
+
+  // ITERATE VOLUMES to create a list of detached volumes tagged for deletion,
+  // and to find all point-in-time snapshots for each volume
+  // (if a volume is slated for deletion, its PIT snapshots will be deleted in
+  // the following cycle)
   _.forEach(results.Volumes.Volumes, function(volume) {
-  
+
+    var volCreateTimeStamp = moment(volume.CreateTime).unix();
+    // select available (detached) volumes created more than 2 days ago
+    if (volume.State == 'available' && volSelectTimeLimiter > volCreateTimeStamp) {
+
+      // check if volume that otherwise would be deletable is excluded by a DND tag
+      if (_.find(volume.Tags, { 'Key': 'DND', 'Value': 'true' })) {
+        volsExcludedByDND.push(volume.VolumeId);
+      }
+      // .. otherwise include the volume on the deletion list
+      else {
+        delVolCollector.push(volume.VolumeId);
+      }
+    }
+    
     var pitSnapshotsCollector = new Set();
     // iterate snapshots for each volume; save found matches
     _.forEach(results.Snapshots.Snapshots, function(snapshot) {
@@ -206,24 +245,134 @@ function(err, results) {
   var actuallyDeletablePitSnapshots = _.difference(deletablePitSnapshots, activeSourceSnapshotsArr);
 
   // exclude snapshots marked explicitly to be saved with "DND:true" tag
-  actuallyDeletablePitSnapshots = _.difference(actuallyDeletablePitSnapshots, excludedByDND);
-  orphanCollectorArr = _.difference(orphanCollectorArr, excludedByDND);
+  actuallyDeletablePitSnapshots = _.difference(actuallyDeletablePitSnapshots, snapsExcludedByDND);
+  orphanSnapCollectorArr = _.difference(orphanSnapCollectorArr, snapsExcludedByDND);
 
   // make sure there are no duplicates (to prevent errors at deletion time)
   deletablePitSnapshots = _.uniq(deletablePitSnapshots);
   activeSourceSnapshotsArr = _.uniq(activeSourceSnapshotsArr);
   actuallyDeletablePitSnapshots = _.uniq(actuallyDeletablePitSnapshots);
-  orphanCollectorArr = _.uniq(orphanCollectorArr);
+  orphanSnapCollectorArr = _.uniq(orphanSnapCollectorArr);
 
-  // load prior/pending deletables (state file)
-  var priorDeletables = loadState();
+  // combine remaining deletable PIT snapshots and orphans
+  var currentlyDeletableSnaps = _.union(actuallyDeletablePitSnapshots, orphanSnapCollectorArr);
 
-  // exclude from saved state any snapshots that have been marked "DND:true" since the state was written
-  priorDeletables = priorDeletables.filter(function(el) {
-    return excludedByDND.indexOf(el.SnapshotId) === -1;
+
+  /* SNAPSHOT PROCESSING */
+
+  // load prior/pending deletables (the state files)
+  var priorDeletableSnaps = loadState(snapStateFile);
+
+//DEBUG
+  console.log('THE PERSISTED SNAPSHOT STATE (' + priorDeletableSnaps.length + '):\n' + util.inspect(priorDeletableSnaps, {showHidden: false, depth: null}));
+  console.log('\n');
+//DEBUG
+
+  // make sure the snapshots in the saved state are still current;
+  // first exclude from saved state any snapshots that have been marked "DND:true" since the state was written
+  priorDeletableSnaps = priorDeletableSnaps.filter(function(el) {
+    return snapsExcludedByDND.indexOf(el.SnapshotId) === -1;
   });
-  // then compact the array as any deletions don't change the array length (i.e. "..item,,item..")
-  priorDeletables = _.compact(priorDeletables);
+  // compact the array to remove any empty spots as deletions don't change the array length (i.e. "..item,,item..")
+  priorDeletableSnaps = _.compact(priorDeletableSnaps);
+
+  // create an independent copy (by value) as the deletions in the array will only be for detection
+  var priorDeletableSnapsCheckArr = [...priorDeletableSnaps];
+
+  // remove all snapshots currently marked for deletion (superset) from the previously marked for deletion (the state file, subset)
+  priorDeletableSnapsCheckArr = deleteBy(currentlyDeletableSnaps, priorDeletableSnapsCheckArr, 'SnapshotId', 'SnapshotId', 'delby1delarr2');
+
+  // if any items remain, they must be removed from the state (they have been either manually deleted, or a volume has been created from them)
+  if (priorDeletableSnapsCheckArr.length) {
+
+    var priorDeletableSnapsDelsArr = priorDeletableSnapsCheckArr.map(function(obj) {
+     return obj.SnapshotId;
+    });
+    priorDeletableSnaps = priorDeletableSnaps.filter(function(el) {
+      return priorDeletableSnapsDelsArr.indexOf(el.SnapshotId) === -1;
+    });
+
+    // compact the array to remove any empty spots as deletions don't change the array length (i.e. "..item,,item..")
+    priorDeletableSnaps = _.compact(priorDeletableSnaps);
+  }
+
+  // remove all snapshots previously marked for deletion from the current deletables (so that just the new ones can be combined in the saved state)
+  currentlyDeletableSnaps = deleteBy(priorDeletableSnaps, currentlyDeletableSnaps, 'SnapshotId', 'SnapshotId', 'delby2delarr1');
+
+  // create pending delete object to add the new items to the persisted state
+  var pendingSnapDels = [];
+  for (var s of currentlyDeletableSnaps) {
+    deleteObj = { 'SnapshotId': s, 'DeletionTime': deletionTime };
+    pendingSnapDels.push(deleteObj);
+  }  
+
+  // combine snap state with the new deletable snapshots
+  var newSnapDelState = _.union(priorDeletableSnaps, pendingSnapDels);
+
+  // make sure there are no duplicates
+  newSnapDelState = _.uniqBy(newSnapDelState, function(snap) { return snap.SnapshotId; });
+
+  // save/persist state
+  saveState(newSnapDelState, snapStateFile);
+
+
+  /* VOLUME PROCESSING */
+
+  // load prior/pending deletables (the state files)
+  var priorDeletableVols = loadState(volStateFile);
+
+//DEBUG
+  console.log('THE PERSISTED VOLUME STATE (' + priorDeletableVols.length + '):\n' + util.inspect(priorDeletableVols, {showHidden: false, depth: null}));
+  console.log('\n');
+//DEBUG
+
+  // make sure the volumes in the saved state are still current;
+  // first exclude from saved state any volumes that have been marked "DND:true" since the state was written
+  priorDeletableVols = priorDeletableVols.filter(function(el) {
+    return volsExcludedByDND.indexOf(el.VolumeId) === -1;
+  });
+  // compact the array to remove any empty spots as deletions don't change the array length (i.e. "..item,,item..")
+  priorDeletableVols = _.compact(priorDeletableVols);
+
+  // create an independent copy (by value) as the deletions in the array will only be for detection
+  var priorDeletableVolsCheckArr = [...priorDeletableVols];
+
+  // remove all volumes currently marked for deletion (superset) from the previously marked for deletion (the state file, subset)
+  priorDeletableVolsCheckArr = deleteBy(delVolCollector, priorDeletableVolsCheckArr, 'VolumeId', 'VolumeId', 'delby1delarr2');
+
+  // if any items are remain, they must be removed from the state (they have been manually deleted)
+  if (priorDeletableVolsCheckArr.length) {
+
+    var priorDeletableVolsDelsArr = priorDeletableVolsCheckArr.map(function(obj) {
+     return obj.VolumeId;
+    });
+    priorDeletableVols = priorDeletableVols.filter(function(el) {
+      return priorDeletableVolsDelsArr.indexOf(el.VolumeId) === -1;
+    });
+
+    // compact the array to remove any empty spots as deletions don't change the array length (i.e. "..item,,item..")
+    priorDeletableVols = _.compact(priorDeletableVols);
+  }
+
+  // remove all volumes previously marked for deletion from the current deletables (so that just the new ones can be combined in the saved state)
+  var currentlyDeletableVols = deleteBy(priorDeletableVols, delVolCollector, 'VolumeId', 'VolumeId', 'delby2delarr1');
+
+  // create pending delete object to add the new items to the persisted state
+  var pendingVolDels = [];
+  for (var s of currentlyDeletableVols) {
+    deleteObj = { 'VolumeId': s, 'DeletionTime': deletionTime };
+    pendingVolDels.push(deleteObj);
+  }  
+
+  // combine snap state with the new deletable volumes
+  var newVolDelState = _.union(priorDeletableVols, pendingVolDels);
+
+  // make sure there are no duplicates
+  newVolDelState = _.uniqBy(newVolDelState, function(volume) { return volume.VolumeId; });
+
+  // save/persist state
+  saveState(newVolDelState, volStateFile);
+
 
   console.log('ALL DELETABLE POINT-IN-TIME SNAPSHOTS (' + deletablePitSnapshots.length + '):\n' + util.inspect(deletablePitSnapshots, {showHidden: false, depth: null}));
   console.log('\n');  
@@ -232,43 +381,25 @@ function(err, results) {
   console.log('**NOTE: all point-in-time snapshots less exclusions is NOT equal to the number of actually deletable snapshots, because for many source snapshots their source no longer exists, and hence they\'re not on the PIT list anyway! DND exclusions may also affect the final number of deletable PIT snapshots.\n');
   console.log('DELETABLE POINT-IN-TIME SNAPSHOTS AFTER EXCLUSIONS (' + actuallyDeletablePitSnapshots.length + '):\n' + util.inspect(actuallyDeletablePitSnapshots, {showHidden: false, depth: null}));
   console.log('\n');
-  console.log('ORPHANS (' + orphanCollectorArr.length + ') TO BE DELETED (WITH DNDs EXCLUDED):\n' + util.inspect(orphanCollectorArr, {showHidden: false, depth: null}));
+  console.log('ORPHANS (' + orphanSnapCollectorArr.length + ') TO BE DELETED (WITH DNDs EXCLUDED):\n' + util.inspect(orphanSnapCollectorArr, {showHidden: false, depth: null}));
   console.log('\n');
   console.log('SOURCE SNAPSHOT DETAIL (KEEPING):\n' + util.inspect(sourceSnapshotDetail, {showHidden: false, depth: null}));
   console.log('\n');
   console.log('SOURCE VOLUME DETAIL (KEEPING):\n' + util.inspect(sourceVolumeDetail, {showHidden: false, depth: null}));
   console.log('\n');
-
-  // combine remaining deletable PIT snapshots and orphans
-  var allCurrentlyDeletableSnapshots = _.union(actuallyDeletablePitSnapshots, orphanCollectorArr);
-  
-  console.log('ALL CURRENTLY DELETABLE SNAPSHOTS (' + allCurrentlyDeletableSnapshots.length + '):\n' + util.inspect(allCurrentlyDeletableSnapshots, {showHidden: false, depth: null}));
+  console.log('NEW DELETABLE SNAPSHOTS (' + currentlyDeletableSnaps.length + '):\n' + util.inspect(currentlyDeletableSnaps, {showHidden: false, depth: null}));
+  console.log('\n');
+  console.log('NEW DELETABLE VOLUMES (' + delVolCollector.length + '):\n' + util.inspect(delVolCollector, {showHidden: false, depth: null}));
   console.log('\n');
 
-  // remove all snapshots previously marked for deletion from the current list
-  allCurrentlyDeletableSnapshots = deleteBy(priorDeletables, allCurrentlyDeletableSnapshots, 'SnapshotId', 'SnapshotId');
+  console.log('SNAPSHOTS EXCLUDED BY DND TAG (' + snapsExcludedByDND.length + '):\n' + util.inspect(snapsExcludedByDND, {showHidden: false, depth: null}));
+  console.log('\n');
+  console.log('VOLUMES EXCLUDED BY DND TAG (' + volsExcludedByDND.length + '):\n' + util.inspect(volsExcludedByDND, {showHidden: false, depth: null}));
+  console.log('\n');
 
-  console.log('CURRENT LIST AFTER SAVED STATE HAS BEEN DELETED: ' + util.inspect(allCurrentlyDeletableSnapshots, {showHidden: false, depth: null}));
+  console.log('THE NEW SNAPSHOT STATE (' + newSnapDelState.length + '):\n' + util.inspect(newSnapDelState, {showHidden: false, depth: null}));
+  console.log('\n');
+  console.log('THE NEW VOLUME STATE (' + newVolDelState.length + '):\n' + util.inspect(newVolDelState, {showHidden: false, depth: null}));
+  console.log('\n');
 
-  // create pending delete object to add to the state
-  var pendingDelete = [];
-  var deleteObj = {};
-  var deletionTime = moment().add(1, 'week').unix();
-  
-  for (var s of allCurrentlyDeletableSnapshots) {
-    deleteObj = { 'SnapshotId': s, 'DeletionTime': deletionTime };
-    pendingDelete.push(deleteObj);
-  }  
-
-  // combine state with the new deletable snapshots
-  var newState = _.union(priorDeletables, pendingDelete);
-
-  console.log('now saving the previous state plus ' + pendingDelete.length + ' new items');
-
-  // save/persist state
-  saveState(newState);
-
-  console.log('NEW STATE:\n');
-  console.log(util.inspect(newState, {showHidden: false, depth: null}));
-  
 });
